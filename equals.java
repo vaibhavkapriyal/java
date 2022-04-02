a86de9dde08e61b75bb90dd7ea132a0358ac8030
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 13:57:29 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,38 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Maninder";
+		String s2="Vaibhav";
+		System.out.println(s.equals(s1));
+		System.out.println(s.equals(s2));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
