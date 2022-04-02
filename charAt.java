98ee53cee33134f1bbb9bdf1c7374a1d75d8759c
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 13:55:01 +0530

    Create charAt.java

---
+++ charAt.java
@@ -0,0 +1,11 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Neha";
+		String s2="Vaibhav";
+		char r= s.charAt(6);
+		System.out.println(r);
+	}
+}
