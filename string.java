5e7ebaae6024a8aae414f301180aebb00b9d8c58
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 13:49:50 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,12 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Neha";
+		String s2="Vaibhav";
+		System.out.println(s.length());
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+	}
+}
