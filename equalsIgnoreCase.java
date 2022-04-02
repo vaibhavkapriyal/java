b3b87858f59a889e4ffca8e8ceb5efc9f6a831c5
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 14:03:47 +0530

    Create equalsIgnoreCase.java

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,39 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Maninder";
+		String s2="Vaibhav";
+		String s3="VAIbhAv";
+		System.out.println(s.equals(s1));
+		System.out.println(s3.equalsIgnoreCase(s2));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
