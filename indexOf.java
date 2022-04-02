edac3788f21370b9ae9113156286d3abd5ad754c
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 14:08:31 +0530

    Create indexOf.java

---
+++ indexOf.java
@@ -0,0 +1,12 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Maninder";
+		String s2="Vaibhav";
+		String s3="VAIbhAv";
+		System.out.println(s.indexOf('n'));
+		System.out.println(s3.indexOf('q'));
+	}
+}
