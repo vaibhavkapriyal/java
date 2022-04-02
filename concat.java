380476724451d5f860ec5a194c73a7a8da3622c6
Sammei <91269085+vaibhavkapriyal@users.noreply.github.com>
2022-04-02 13:52:17 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,11 @@
+class string
+{
+	public static void main(String args[])
+	{
+		String s="Maninder";
+		String s1="Neha";
+		String s2="Vaibhav";
+		System.out.println(s.concat(s1));
+		System.out.println(s.length());
+	}
+}
